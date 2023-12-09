9128d6c12f9de4cb4a1fed444d9dc16068eb7b5a
shz-code <web.shahidul.alam@gmail.com>
2023-12-10 03:56:00 +0600

    v0.5.2 (Package Design)

--- package/urls.py
+++ package/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "package"
 
 urlpatterns = [
-    # path('', views.index , name='index'),
+    path('', views.index , name='index'),
+    path('edit/<int:pk>', views.edit , name='edit'),
 ]
