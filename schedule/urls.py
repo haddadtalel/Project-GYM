e1e30d0d94a5038e5c7b40f910897907c6f3bb5f
shz-code <web.shahidul.alam@gmail.com>
2023-12-10 19:26:37 +0600

    v0.7.7 (Schedule Module)

--- schedule/urls.py
+++ schedule/urls.py
@@ -6,4 +6,5 @@ app_name = "schedule"
 urlpatterns = [
     path('', views.index , name='index'),
     path('edit/<int:pk>', views.edit , name='edit'),
+    path('delete/<int:pk>', views.delete , name='delete'),
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -1,8 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
+from .models import Schedule,TYPE_CHOICE,DAY_CHOICES
 # Create your views here.
 def index(request):
-    return render(request,'schedule/index.html')
+    schedules = Schedule.objects.all().order_by("-id")
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        day = request.POST.get("day")
+        start_time = request.POST.get("start_time")
+        end_time = request.POST.get("end_time")
+
+        Schedule.objects.create(
+            name = name,
+            day = day,
+            start_time = start_time,
+            end_time = end_time
+        )
+
+    context = {
+        'schedules':schedules,
+        'DAY_CHOICES':DAY_CHOICES,
+        'TYPE_CHOICE':TYPE_CHOICE
+    }
+    return render(request,'schedule/index.html',context)
 
 def edit(request,pk):
-    return render(request,"schedule/edit.html")
+    schedule = Schedule.objects.get(id = pk)
+
+    print(schedule)
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        day = request.POST.get("day")
+        start_time = request.POST.get("start_time")
+        end_time = request.POST.get("end_time")
+
+        schedule.name = name
+        schedule.day = day
+        if start_time != "":
+            schedule.start_time = start_time
+        if end_time != "":
+            schedule.end_time = end_time
+
+        schedule.save()
+        return redirect("/schedule/")
+    context = {
+        'schedule':schedule,
+        'DAY_CHOICES':DAY_CHOICES,
+        'TYPE_CHOICE':TYPE_CHOICE
+    }
+
+    return render(request,"schedule/edit.html",context)
+
+def delete(request,pk):
+    Schedule.objects.get(id = pk).delete()
+    return redirect("/schedule/")
