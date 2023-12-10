cccd44b60ca1f54e1f18cec7932e317e37f337d6
shz-code <web.shahidul.alam@gmail.com>
2023-12-10 14:55:49 +0600

    v0.7.5 (Equipment CRUD)

--- equipment/models.py
+++ equipment/models.py
@@ -18,13 +18,20 @@ class Equipment(models.Model):
     maintenanceDate = models.DateField(_("Maintenance Date"), null=True,blank=True)
     is_available =  models.BooleanField(_("Availability Status"),default=True)
     url =  models.TextField(_("Url"),null=True,blank=True)
+    
+    price = models.FloatField(_("Price"),null=True,blank=True)
+    brand = models.CharField(_("Brand"),max_length=50, null=True,blank=True)
 
     def __str__(self) -> str:
         return f'{self.name}'
+    
+    def get_eqTypes(self):
+        eqType = self.equipmentType.all()
+        return eqType    
 
 class EquipmentActivityTrack(models.Model):
-    start_time = models.DateField(_("Booking start"),null=True,blank=True)
-    end_time = models.DateField(_("Booking end"),null=True,blank=True)
+    start_time = models.DateTimeField(_("Booking start"),null=True,blank=True)
+    end_time = models.DateTimeField(_("Booking end"),null=True,blank=True)
 
     booked_by = models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
     booked_for =models.ForeignKey('member.Member', on_delete=models.CASCADE,null=True,blank=True)
--- equipment/views.py
+++ equipment/views.py
@@ -1,11 +1,83 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
+from .models import Equipment,EquipmentType,EquipmentActivityTrack
+from datetime import datetime
 # Create your views here.
 def index(request):
-    return render(request,'equipment/index.html')
+    eqs = Equipment.objects.all()
+    eqTypes = EquipmentType.objects.all()
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        brand = request.POST.get("brand")
+        costCondition = request.POST.get("costCondition")
+        serial = request.POST.get("serial")
+        price = request.POST.get("price")
+
+        eqTypes_res = request.POST.getlist("eqType")
+
+        print(costCondition)
+        print(eqTypes_res)
+
+        if costCondition == "store":
+            pass
+
+        eq = Equipment.objects.create(
+            name = name,
+            brand = brand,
+            serial = serial,
+            price = price,
+            maintenanceDate = datetime.now()
+        )
+        for eqT in eqTypes_res:
+            eq.equipmentType.add(eqT)
+        
+    context = {
+        'eqs':eqs,
+        'eqTypes':eqTypes
+    }
+    return render(request,'equipment/index.html',context)
 
 def edit(request,pk):
-    return render(request,"equipment/edit.html")
+    eq = Equipment.objects.get(id = pk)
+    eqTypes = EquipmentType.objects.all()
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        brand = request.POST.get("brand")
+        eqTypes_res = request.POST.getlist("eqType")
+
+        eq.name = name
+        eq.brand = brand
+
+            
+        eq.save()
+
+        eq.equipmentType.clear()
+        for eqT in eqTypes_res:
+            eq.equipmentType.add(eqT)
+
+    context = {
+        'eq':eq,
+        'eqTypes':eqTypes
+    }
+    return render(request,"equipment/edit.html",context)
 
 def book(request):
     return render(request,"equipment/book.html")
+
+def deactivate(request,pk):
+    eq = Equipment.objects.get(id = pk)
+    eq.is_available = False
+    eq.save()
+    return redirect("/equipment/")
+
+def active(request,pk):
+    eq = Equipment.objects.get(id = pk)
+    eq.is_available = True
+    eq.save()
+    return redirect("/equipment/")
+
+def delete(request,pk):
+    Equipment.objects.get(id = pk).delete()
+    return redirect("/equipment/")
